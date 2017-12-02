19f2b4e4882648da775099fda2732c52bf3becbf
ph <phpellerin@gmail.com>
2017-12-01 21:02:09 -0500

    Day 1

---
+++ day1/2/day.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil || len(input) == 0 {
+		panic("Missing values through stdin.")
+	}
+
+	strInput := string(input[:])
+	fmt.Printf("Code: %s\nResolve:\n%d\n", strInput, resolve(strInput))
+}
+
+func resolve(input string) int {
+	chars := strings.Split(input, "")
+
+	numbers := make([]int, 0)
+	for _, c := range chars {
+		if i, err := strconv.Atoi(c); err == nil {
+			numbers = append(numbers, i)
+		}
+	}
+
+	sum := 0
+	for i := range numbers {
+		n := (i + len(numbers)/2) % len(numbers)
+		if numbers[i] == numbers[n] {
+			sum += numbers[i]
+		}
+	}
+	return sum
+}
